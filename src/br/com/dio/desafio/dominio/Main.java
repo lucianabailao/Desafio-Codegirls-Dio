2fa7bd9f9229af4b25945f4b2151699a0a15ce71
Luciana <lucianasbailao@hotmail.com>
2022-07-17 23:24:29 -0300

    Criação de classe e métodos

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,28 +1,27 @@
 package br.com.dio.desafio.dominio;
-
-
 import java.time.LocalDate;
+
 
 public class Main {
     public static  void main(String[] args) {
 
     Curso curso1 = new Curso();
-    curso1.setTitulo("Curso Java");
-    curso1.setDescricao("Descrição do Curso Java");
+    curso1.setTitulo(" Curso Java");
+    curso1.setDescricao(" Descrição do Curso Java");
     curso1.setCargaHoraria(8);
 
     Curso curso2 = new Curso();
-    curso2.setTitulo("Curso JS");
-    curso2.setDescricao("Descrição do Curso JS");
+    curso2.setTitulo(" Curso JS");
+    curso2.setDescricao(" Descrição do Curso JS");
     curso2.setCargaHoraria(4);
-
+        
     Mentoria mentoria = new Mentoria();
-    mentoria.setTitulo("Mentoria de Java");
-    mentoria.setDescricao("Descrição mentoria java");
+    mentoria.setTitulo(" Mentoria de Java");
+    mentoria.setDescricao(" Descrição mentoria java");
     mentoria.setData(LocalDate.now());
 
-    System.out.println(curso1);
-    System.out.println(curso2);
-    System.out.println(mentoria);
+    System.out.println( curso1);
+    System.out.println( curso2);
+    System.out.println( mentoria);
     }
 }
